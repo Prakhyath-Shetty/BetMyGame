401a922d74fdde44c3ef5eef86186d0e732f0d32
Parjanya Kumar <parjanya.kumar@aykansoft.com>
2020-10-11 21:14:38 +0530

    Start changes

--- SourceCode/BackendServer/build/repository/index.js
+++ SourceCode/BackendServer/build/repository/index.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRepository = exports.RepositoryBase = void 0;
 var RepositoryBase_1 = require("./RepositoryBase");
 Object.defineProperty(exports, "RepositoryBase", { enumerable: true, get: function () { return RepositoryBase_1.RepositoryBase; } });
 var UserRepository_1 = require("./UserRepository");
